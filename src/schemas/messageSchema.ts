c00b9677f57e753e2fa44663178bf63792dda639
Jatin Agrawal <jatinagrawal0987654321@gmail.com>
2025-06-05 17:51:23 +0530

    final

--- src/schemas/messageSchema.ts
+++
@@ -1,9 +0,0 @@
-import {z} from "zod";
-
-export const messageSchema = z.object({
-    content: z
-    .string()
-    .min(10, {message: "Message must be at least 10 character long"})
-    .max(500, {message: "Message must be at most 500 characters long"})
-
-});
